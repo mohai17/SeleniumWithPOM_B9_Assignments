795c8cc88245bf221596d96d250607757075f5bf
mohai <mohai.cse@gmail.com>
2024-06-12 05:14:42 +0600

    RegisterPage Test is done.

--- src/test/java/com/parabank/parasoft/pages/RegisterPage.java
+++ src/test/java/com/parabank/parasoft/pages/RegisterPage.java
@@ -1,8 +1,84 @@
 package com.parabank.parasoft.pages;
 
-public class RegisterPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-//    registerSucceed()
-//    registerFailedWithAnyBlankField()
-//    registerFailedWithAllBlankField()
+public class RegisterPage extends BasePage{
+
+    public RegisterPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public RegisterPage registerLinkClick(){
+        getPageElement(By.cssSelector("a[href='register.htm']")).click();
+        return this;
+    }
+
+    public RegisterPage firstName(String firstname){
+        getPageElement(By.cssSelector("input[id='customer.firstName']")).sendKeys(firstname);
+        return this;
+    }
+
+    public RegisterPage lastName(String lastname){
+        getPageElement(By.cssSelector("input[id='customer.lastName']")).sendKeys(lastname);
+        return this;
+    }
+
+    public RegisterPage address(String address){
+        getPageElement(By.cssSelector("input[id='customer.address.street']")).sendKeys(address);
+        return this;
+    }
+    public RegisterPage city(String city){
+        getPageElement(By.cssSelector("input[id='customer.address.city']")).sendKeys(city);
+        return this;
+    }
+    public RegisterPage state(String state){
+        getPageElement(By.cssSelector("input[id='customer.address.state']")).sendKeys(state);
+        return this;
+    }
+    public RegisterPage zipCode(String zipcode){
+        getPageElement(By.cssSelector("input[id='customer.address.zipCode']")).sendKeys(zipcode);
+        return this;
+    }
+    public RegisterPage phoneNumber(String phoneNumber) {
+        getPageElement(By.cssSelector("input[id='customer.phoneNumber']")).sendKeys(phoneNumber);
+        return this;
+    }
+
+    public RegisterPage socialSecurityNumber(String ssn){
+        getPageElement(By.cssSelector("input[id='customer.ssn']")).sendKeys(ssn);
+        return this;
+    }
+
+
+    public RegisterPage userName(String userName){
+        getPageElement(By.cssSelector("input[id='customer.username']")).sendKeys(userName);
+        return this;
+    }
+    public RegisterPage password(String password){
+        getPageElement(By.cssSelector("input[id='customer.password']")).sendKeys(password);
+        return this;
+    }
+    public RegisterPage confirmPassword(String confirmPassword){
+        getPageElement(By.cssSelector("input[id='repeatedPassword']")).sendKeys(confirmPassword);
+        return this;
+    }
+
+    public RegisterPage registerButtonClick(){
+        getPageElement(By.cssSelector("input[value='Register']")).click();
+        return this;
+    }
+
+    public boolean isSucceed(){
+
+        return !getPageElements(By.linkText("Log Out")).isEmpty();
+//        return !getPageElements(By.partialLinkText("successfully")).isEmpty();
+
+    }
+
+
+    public boolean hasError(){
+        return getPageElements(By.className("error")).size()<=10;
+    }
+
 }
--- src/test/java/com/parabank/parasoft/testCases/RegisterTest.java
+++ src/test/java/com/parabank/parasoft/testCases/RegisterTest.java
@@ -1,4 +1,66 @@
 package com.parabank.parasoft.testCases;
 
-public class RegisterTest {
+import com.parabank.parasoft.pages.RegisterPage;
+import com.thedeanda.lorem.LoremIpsum;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RegisterTest extends BaseTest{
+
+    @Test(priority = 0)
+    public void registerFailedWithAnyBlankField(){
+
+        LoremIpsum lorem = new LoremIpsum();
+        String fName = lorem.getFirstName();
+
+        RegisterPage registerPage = basePage.getInstance(RegisterPage.class);
+        boolean actual = registerPage.registerLinkClick()
+                .firstName(fName)
+                .lastName(lorem.getLastName())
+                .city(lorem.getCity())
+                .state(lorem.getStateFull())
+                .zipCode(lorem.getZipCode())
+                .phoneNumber(lorem.getPhone())
+                .userName(fName)
+                .password(fName)
+                .confirmPassword(fName)
+                .registerButtonClick()
+                .hasError();
+
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test(priority = 1)
+    public void registerFailedWithAllBlankField(){
+        RegisterPage registerPage = basePage.getInstance(RegisterPage.class);
+        boolean actual = registerPage.registerLinkClick().registerButtonClick().hasError();
+        Assert.assertTrue(actual);
+    }
+
+    @Test(priority = 2)
+    public void registerSucceed(){
+
+        LoremIpsum lorem = new LoremIpsum();
+        String fName = lorem.getFirstName();
+
+        RegisterPage registerPage = basePage.getInstance(RegisterPage.class);
+        boolean actual = registerPage.registerLinkClick()
+                .firstName(fName)
+                .lastName(lorem.getLastName())
+                .address(lorem.getWords(3))
+                .city(lorem.getCity())
+                .state(lorem.getStateFull())
+                .zipCode(lorem.getZipCode())
+                .phoneNumber(lorem.getPhone())
+                .socialSecurityNumber("123-4560-789")
+                .userName(fName)
+                .password(fName)
+                .confirmPassword(fName)
+                .registerButtonClick()
+                .isSucceed();
+
+        Assert.assertTrue(actual);
+
+    }
 }
