70492829fab4800ecaa618cf9cef4f94c7efe47d
mohai <mohai.cse@gmail.com>
2024-06-14 05:14:51 +0600

    OpenAccountPage Test is done.

--- src/test/java/com/parabank/parasoft/pages/OpenAccountPage.java
+++ src/test/java/com/parabank/parasoft/pages/OpenAccountPage.java
@@ -1,6 +1,74 @@
 package com.parabank.parasoft.pages;
 
-public class OpenAccountPage {
-//    savingAccountOpen()
-//    checkingAccountOpen()
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class OpenAccountPage extends BasePage{
+
+    public OpenAccountPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public LoginPage userName(String username){
+
+        getPageElement(By.cssSelector("input[name='username']")).sendKeys(username);
+        return getInstance(LoginPage.class);
+    }
+
+    public LoginPage password(String password){
+
+        getPageElement(By.cssSelector("input[name='password']")).sendKeys(password);
+        return getInstance(LoginPage.class);
+    }
+
+    public AccountOverviewPage loginButtonClick(){
+
+        getPageElement(By.cssSelector("input[type='submit']")).click();
+        return getInstance(AccountOverviewPage.class);
+
+    }
+
+    public OpenAccountPage openAccountLink(){
+
+        getPageElement(By.cssSelector("a[href='openaccount.htm']")).click();
+        return this;
+    }
+
+    public OpenAccountPage typeOfAccount(String accountType){
+
+        WebElement element = getPageElement(By.cssSelector("select[id='type']"));
+        Select select = new Select(element);
+        select.selectByVisibleText(accountType);
+
+        return this;
+    }
+
+    public OpenAccountPage existingAccountId(String accountID){
+        WebElement element = getPageElement(By.cssSelector("select[id='fromAccountId']"));
+        Select select = new Select(element);
+        select.selectByVisibleText(accountID);
+
+        return this;
+    }
+
+    public OpenAccountPage openAccountButtonClick(){
+        getPageElement(By.cssSelector("input[value='Open New Account']")).click();
+        return this;
+    }
+
+    public boolean isOpenAccountSucceed(){
+
+        return !getPageElements(By.cssSelector("a[id='newAccountId']")).isEmpty();
+    }
+
+
+
+    public OpenAccountPage logout(){
+        getPageElement(By.cssSelector("a[href='logout.htm']")).click();
+        return this;
+    }
+
+
 }
--- src/test/java/com/parabank/parasoft/testCases/OpenAccountTest.java
+++ src/test/java/com/parabank/parasoft/testCases/OpenAccountTest.java
@@ -1,4 +1,55 @@
 package com.parabank.parasoft.testCases;
 
-public class OpenAccountTest {
+import com.parabank.parasoft.pages.LoginPage;
+import com.parabank.parasoft.pages.OpenAccountPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OpenAccountTest extends BaseTest{
+
+    @Test
+
+    public void openCheckingAccountSucceed(){
+
+        LoginPage loginPage = basePage.getInstance(LoginPage.class);
+        loginPage.userName(getUserName())
+                 .password(getPassword())
+                 .loginButtonClick();
+
+        OpenAccountPage openAccountPage = basePage.getInstance(OpenAccountPage.class);
+
+        boolean actual = openAccountPage.openAccountLink()
+                .typeOfAccount("CHECKING")
+                .openAccountButtonClick()
+                .isOpenAccountSucceed();
+
+        Assert.assertTrue(actual);
+
+        openAccountPage.logout();
+
+    }
+
+    @Test
+    public void openSavingsAccountSucceed(){
+
+        LoginPage loginPage = basePage.getInstance(LoginPage.class);
+        loginPage.userName(getUserName())
+                .password(getPassword())
+                .loginButtonClick();
+
+        OpenAccountPage openAccountPage = basePage.getInstance(OpenAccountPage.class);
+
+        boolean actual = openAccountPage.openAccountLink()
+                .typeOfAccount("SAVINGS")
+                .openAccountButtonClick()
+                .isOpenAccountSucceed();
+
+        Assert.assertTrue(actual);
+
+        openAccountPage.logout();
+    }
+
+
+
+
 }
