f5e97a6643902d0c67d2be849ad91d76fa9196fe
mohai <mohai.cse@gmail.com>
2024-06-15 03:34:02 +0600

    UpdateContactInfo is done.

--- src/test/java/com/parabank/parasoft/pages/UpdateContactInfo.java
+++ src/test/java/com/parabank/parasoft/pages/UpdateContactInfo.java
@@ -1,6 +1,121 @@
 package com.parabank.parasoft.pages;
 
-public class UpdateContactInfo {
-//    updateProfileSucceed()
-//    updateProfileFailed()
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class UpdateContactInfo extends BasePage{
+
+    public UpdateContactInfo(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public LoginPage userName(String userName){
+
+        getPageElement(By.cssSelector("input[name='username']")).sendKeys(userName);
+        return getInstance(LoginPage.class);
+    }
+
+    public LoginPage password(String password){
+
+        getPageElement(By.cssSelector("input[name='password']")).sendKeys(password);
+        return getInstance(LoginPage.class);
+    }
+
+    public AccountOverviewPage loginButtonClick(){
+
+        getPageElement(By.cssSelector("input[type='submit']")).click();
+        return getInstance(AccountOverviewPage.class);
+
+    }
+
+    public UpdateContactInfo updateContactInfoLinkClick(){
+
+        getPageElement(By.cssSelector("a[href='updateprofile.htm']")).click();
+
+        return this;
+
+    }
+
+    public UpdateContactInfo firstNameField(String firstName){
+
+       WebElement element = getPageElement(By.cssSelector("input[id='customer.firstName']"));
+       element.clear();
+       element.sendKeys(firstName);
+
+       return this;
+    }
+
+    public UpdateContactInfo lastNameField(String lastName){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.lastName']"));
+        element.clear();
+        element.sendKeys(lastName);
+
+        return this;
+    }
+
+    public UpdateContactInfo streetField(String street){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.address.street']"));
+        element.clear();
+        element.sendKeys(street);
+
+        return this;
+    }
+
+    public UpdateContactInfo cityField(String city){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.address.city']"));
+        element.clear();
+        element.sendKeys(city);
+
+        return this;
+    }
+
+    public UpdateContactInfo stateField(String state){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.address.state']"));
+        element.clear();
+        element.sendKeys(state);
+
+        return this;
+    }
+
+    public UpdateContactInfo zipCodeField(String zipCode){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.address.zipCode']"));
+        element.clear();
+        element.sendKeys(zipCode);
+
+        return this;
+    }
+
+    public UpdateContactInfo phoneNumber(String phoneNumber){
+
+        WebElement element = getPageElement(By.cssSelector("input[id='customer.phoneNumber']"));
+        element.clear();
+        element.sendKeys(phoneNumber);
+
+        return this;
+    }
+
+    public UpdateContactInfo updateButtonClick(){
+
+        getPageElement(By.cssSelector("input[value='Update Profile']")).click();
+
+        return this;
+    }
+
+    public boolean isProfileUpdated(){
+
+        return !getPageElements(By.cssSelector("div[id='updateProfileResult']")).isEmpty();
+
+    }
+
+    public boolean isErrorFound(){
+
+        return getPageElements(By.className("error")).size() <=8;
+    }
+
 }
--- src/test/java/com/parabank/parasoft/testCases/UpdateContactInfoTest.java
+++ src/test/java/com/parabank/parasoft/testCases/UpdateContactInfoTest.java
@@ -1,4 +1,45 @@
 package com.parabank.parasoft.testCases;
 
-public class UpdateContactInfoTest {
+import com.parabank.parasoft.pages.UpdateContactInfo;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UpdateContactInfoTest extends BaseTest{
+
+    @Test
+    public void updateProfileSucceed(){
+
+        UpdateContactInfo update = basePage.getInstance(UpdateContactInfo.class);
+
+        update.userName(getUserName()).password(getPassword()).loginButtonClick();
+
+        boolean actual = update.updateContactInfoLinkClick()
+                .firstNameField(getFirstName())
+                .lastNameField(getLastName())
+                .streetField(getStreet())
+                .cityField(getCity())
+                .stateField(getState())
+                .zipCodeField(getZipCode())
+                .phoneNumber(getPhoneNumber())
+                .updateButtonClick()
+                .isProfileUpdated();
+
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+    public void updateProfileFailedWithAllBlankField()
+    {
+        UpdateContactInfo update = basePage.getInstance(UpdateContactInfo.class);
+
+        update.userName(getUserName()).password(getPassword()).loginButtonClick();
+
+        boolean actual = update.updateContactInfoLinkClick()
+                .updateButtonClick()
+                .isErrorFound();
+
+        Assert.assertTrue(actual);
+    }
+
 }
