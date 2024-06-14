a7de25614ed722c0df7e3cbdb06356a9c9f86546
mohai <mohai.cse@gmail.com>
2024-06-15 04:36:52 +0600

    RequestLoan is done.

--- src/test/java/com/parabank/parasoft/testCases/RequestLoanTest.java
+++ src/test/java/com/parabank/parasoft/testCases/RequestLoanTest.java
@@ -1,4 +1,37 @@
 package com.parabank.parasoft.testCases;
 
-public class RequestLoanTest {
+import com.parabank.parasoft.pages.RequestLoan;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RequestLoanTest extends BaseTest{
+
+    @Test
+    public void loanApprovalSucceed(){
+        RequestLoan requestLoan = basePage.getInstance(RequestLoan.class);
+
+        requestLoan.userName(getUserName()).password(getPassword()).loginButtonClick();
+        boolean actual = requestLoan.requestLoanLinkClick()
+                .loanAmountField(getLoanAmount())
+                .downPaymentField(getDownPayment())
+                .fromAccountField(getAccountId())
+                .applyNowButtonClick()
+                .isLoanApproved();
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void loanApprovalFailedWithBlankField(){
+
+        RequestLoan requestLoan = basePage.getInstance(RequestLoan.class);
+
+        requestLoan.userName(getUserName()).password(getPassword()).loginButtonClick();
+        boolean actual = requestLoan.requestLoanLinkClick()
+                .applyNowButtonClick()
+                .isErrorFound();
+
+        Assert.assertTrue(actual);
+    }
+
 }
