e43ab497d8eda84b61b412230a799265518fa1a9
mohai <mohai.cse@gmail.com>
2024-06-15 08:34:42 +0600

    ContactPage is done.

--- src/test/java/com/parabank/parasoft/pages/Contact.java
+++ src/test/java/com/parabank/parasoft/pages/Contact.java
@@ -1,5 +1,45 @@
 package com.parabank.parasoft.pages;
 
-public class Contact {
-//    customerCare()
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Contact extends BasePage{
+
+    public Contact(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public Contact clickContact(){
+        getPageElement(By.className("contact")).click();
+        return this;
+    }
+
+    public Contact name(String name){
+        getPageElement(By.cssSelector("input[id='name']")).sendKeys(name);
+        return this;
+    }
+
+    public Contact email(String email){
+        getPageElement(By.cssSelector("input[id='email']")).sendKeys(email);
+        return this;
+    }
+//
+//
+    public Contact phone(String phone){
+        getPageElement(By.cssSelector("input[id='phone']")).sendKeys(phone);
+        return this;
+    }
+    public Contact textArea(String comment){
+        getPageElement(By.cssSelector("textarea[id='message']")).sendKeys(comment);
+        return this;
+    }
+    public Contact submitButtonClick(){
+        getPageElement(By.cssSelector("input[value='Send to Customer Care']")).click();
+        return this;
+    }
+    public boolean isPass(){
+
+        return !getPageElements(By.xpath("//div[@id='rightPanel']")).isEmpty();
+    }
 }
